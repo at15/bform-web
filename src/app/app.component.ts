8796c119c7a1bae6366ea6ba225d3a1841206758
at15 <at15@outlook.com>
2016-04-07 16:54:16 +0800

    Init the angular app
    now we can show, this is bform!

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by at15 on 2016/4/7.
+ */
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'bform-app',
+    template: `
+    <h1>This is bform</h1>
+`
+})
+export class AppComponent {
+    
+}
