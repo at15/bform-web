4197f9d7309d21930b646705939fa7e1c33a4703
at15 <at15@outlook.com>
2016-04-16 00:27:35 +0800

    Move template into user login form
    now user login component is almost blank, for #2

--- src/app/user/user.login-form.component.ts
+++ src/app/user/user.login-form.component.ts
@@ -3,12 +3,14 @@
  */
 // TODO: put it inside user.login.component
 import {Component} from 'angular2/core';
+// TODO: deal with ngform later, it's just UI & UE problem after all.
 import {NgForm} from 'angular2/common';
 
 @Component({
     selector:'bform-user-login-form',
-    templateUrl:'./assets/user/user.login-form.html'
+    templateUrl:'./assets/user/user.login-form.html',
+    styleUrls:['./assets/user/user.login-form.css']
 })
 export class UserLoginFormComponent {
-
+    
 }
--- src/app/user/user.login.component.ts
+++ src/app/user/user.login.component.ts
@@ -4,24 +4,26 @@
 
 import {Component} from 'angular2/core';
 import {Router} from 'angular2/router';
+import {UserLoginFormComponent} from './user.login-form.component';
 
 @Component({
     // TODO: is it possible to put this component inside a modal
     selector: 'bform-user-login',
     styleUrls: ['./assets/user/user.login.css'],
-    templateUrl: './assets/user/user.login.html'
+    template: '<bform-user-login-form></bform-user-login-form>',
+    directives: [UserLoginFormComponent]
 })
 export class UserLoginComponent {
-    constructor(private _router:Router){
+    constructor(private _router:Router) {
 
     }
 
-    onSubmit(){
+    onSubmit() {
         console.log('submit');
         this.goToDashBoard();
     }
-    
-    goToDashBoard(){
+
+    goToDashBoard() {
         this._router.navigate(['UserDashboard']);
     }
 
